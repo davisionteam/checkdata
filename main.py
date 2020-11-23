09801dea2b60d3d8bf966b084e6d28ccea28cf2d
Loi Ly <vinhloiit1327@gmail.com>
2020-11-23 20:09:56 +0700

    edit CMND

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import csv
 import glob
 import json
+import math
 import os
 import sys
 from pathlib import Path
+from typing import Dict, List, Optional
 
 import cv2
 import numpy as np
@@ -13,9 +15,9 @@ from PIL.ImageQt import ImageQt
 from PyQt5.QtCore import QEvent, QSize, Qt, pyqtSignal, qDebug
 from PyQt5.QtGui import (QColor, QFont, QFontMetrics, QGuiApplication, QImage,
                          QImageReader, QImageWriter, QIntValidator, QKeyEvent,
-                         QPainter, QPalette, QPixmap)
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
-                             QMainWindow, QMessageBox, QPushButton, QGroupBox,
+                         QKeySequence, QPainter, QPalette, QPixmap)
+from PyQt5.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QLabel,
+                             QLineEdit, QMainWindow, QMessageBox, QPushButton,
                              QScrollArea, QScrollBar, QShortcut, QSizePolicy,
                              QVBoxLayout, QWidget)
 
@@ -38,7 +40,7 @@ class SwitchSignal(QWidget):
             print('KEy Down')
             self.next.emit()
 
-class Account():
+class Dataset():
     def __init__(self, acc_dir: Path):
         ext_list = ['jpg', 'jpeg', 'png']
         ext_list.extend([x.upper() for x in ext_list])
@@ -46,9 +48,10 @@ class Account():
 
         self.acc_images = sum([sorted(list(acc_dir.glob(pattern))) for pattern in ext_list], [])
         self.acc_jsons = [image.with_suffix('.json') for image in self.acc_images]
+        self.acc_json_diff = [image.with_name(image.stem + '_checked.json') for image in self.acc_images]
         self.accs = []
-        for image_path, json_path in zip(self.acc_images, self.acc_jsons):
-            acc_file = AccountFile(image_path, json_path)
+        for image_path, json_path, json_diff_path in zip(self.acc_images, self.acc_jsons, self.acc_json_diff):
+            acc_file = ImageDir(image_path, json_path, json_diff_path)
             if len(acc_file) > 0:
                 self.accs.append(acc_file)
     
@@ -59,20 +62,24 @@ class Account():
         return len(self.accs)
 
 
-class AccountFile():
-    def __init__(self, image_path: Path, json_path: Path):
+class ImageDir():
+    def __init__(self, image_path: Path, json_path: Path, json_diff_path: Path):
         self.image_path = image_path
         self.json_path = json_path
+        self.json_diff_path = json_diff_path
 
         self.image: Image.Image = Image.open(image_path)
-        self.textlines = json.load(open(json_path, encoding='utf8'))
-        self.check_flags = [0] * len(self.textlines)
+        self.json_gt = json.load(open(json_path, encoding='utf8'))
+        self.textlines_diff = json.load(open(json_diff_path, encoding='utf8'))
+        self.check_flags = [0] * len(self.textlines_diff)
 
     def __getitem__(self, idx):
-        obj = self.textlines[idx]
+        obj = self.textlines_diff[idx]
         predict_text = obj['predict_text'].strip()
-        labling_text = obj['labling_text'].strip()
+        label_text = obj['label_text'].strip()
         points = obj['coords']
+        ref = self.find_ref_by_coords(points)
+        textline = TextLine(ref, predict_text)
 
         if isinstance(points, list):
             cv_image = np.array(self.image)
@@ -90,10 +97,67 @@ class AccountFile():
         else:
             print('Unknow type of "coords"')
             exit(-1)
-        return cur_tl_img, predict_text, labling_text
+        return textline, cur_tl_img
+
+    def update_line_value(self, ref: Dict, value: str):
+        ref['value'] = value
+
+    def find_ref_by_coords(self, coords: List) -> Optional[Dict]:
+        for shape in self.json_gt['shapes']:
+            if len(shape['points']) != 4:
+                # we know that textline must have 4 coords
+                continue
+            if self.is_same_coords(shape['points'], coords):
+                return shape
+        return None
+
+    def is_same_coords(self, coords1, coords2) -> bool:
+        if isinstance(coords1, list):
+            coords1 = self.flatten_coords(coords1)
+        else:
+            print('Current only support list type')
+            return False
+        if isinstance(coords2, list):
+            coords2 = self.flatten_coords(coords2)
+        else:
+            print('Current only support list type')
+            return False
+
+        assert len(coords1) == len(coords2)
+        for idx in range(len(coords1)):
+            if abs(coords1[idx] - coords2[idx]) > 2:
+                return False
+        return True
+
+    def flatten_coords(self, coords):
+        coords = [int(val) for coord in coords for val in coord]
+        return coords
 
     def __len__(self):
-        return len(self.textlines)
+        return len(self.textlines_diff)
+
+    def save(self):
+        json.dump(self.json_gt,
+                  open(self.json_path, 'wt', encoding='utf8'),
+                  indent=4,
+                  ensure_ascii=False)
+
+class TextLine():
+    def __init__(self, ref: Dict, predict: str):
+        self.ref = ref
+        self.predict = predict
+
+    @property
+    def textline(self) -> str:
+        return self.ref['value']
+
+    @textline.setter
+    def textline(self, value):
+        self.ref['value'] = value
+
+    @property
+    def coords(self):
+        return self.ref['points']
 
 class App(QMainWindow):
     def __init__(self, acc_dir):
@@ -167,7 +231,7 @@ class App(QMainWindow):
         f.setStyleHint(QFont.Monospace)
         self.label_text.setFont(f)
         self.label_text.setFocus()
-        self.label_text.setReadOnly(True)
+        self.label_text.setReadOnly(False)
         layout.addWidget(self.label_text)
 
         label = QLabel('OCR Predict:')
@@ -183,27 +247,6 @@ class App(QMainWindow):
         self.pred_text.setReadOnly(True)
         layout.addWidget(self.pred_text)
 
-        buttons = QWidget()
-        button_layout = QHBoxLayout()
-        self.correct_button = QPushButton('ĐÚNG')
-        pallete = self.correct_button.palette()
-        pallete.setColor(QPalette.Button, QColor(Qt.green))
-        self.correct_button.setFixedHeight(40)
-        self.correct_button.setPalette(pallete)
-        self.correct_button.clicked.connect(self.on_correct_button_clicked)
-        self.correct_button.setShortcut("1")
-        button_layout.addWidget(self.correct_button)
-        self.incorrect_button = QPushButton('SAI')
-        pallete = self.correct_button.palette()
-        pallete.setColor(QPalette.Button, QColor(Qt.red))
-        self.incorrect_button.setFixedHeight(40)
-        self.incorrect_button.setPalette(pallete)
-        self.incorrect_button.clicked.connect(self.on_incorrect_button_clicked)
-        self.incorrect_button.setShortcut("2")
-        button_layout.addWidget(self.incorrect_button)
-        buttons.setLayout(button_layout)
-        layout.addWidget(buttons)
-
         signal_widget = SwitchSignal()
         signal_widget.next.connect(self.next_image)
         signal_widget.prev.connect(self.prev_image)
@@ -217,7 +260,7 @@ class App(QMainWindow):
 
         self.current_index = 0
 
-        self.account = Account(acc_dir)
+        self.account = Dataset(acc_dir)
 
         if len(self.account) == 0:
             print('Nothing to do! Nice!')
@@ -227,12 +270,25 @@ class App(QMainWindow):
 
         self.need_save = False
         self.acc_file_index = 0
-        self.current_account_file = self.account[0]
+        self.current_image_dir = self.account[0]
         self.total_acc_label.setText(f'{len(self.account) - 1:05d}')
-        self.total_line_label.setText(f'{len(self.current_account_file) - 1:05d}')
+        self.total_line_label.setText(f'{len(self.current_image_dir) - 1:05d}')
         self.set_step(0)
 
         self.rotate_degree = 0
+
+        #######################
+        # Set shortcut
+        #######################
+        shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
+        shortcut.activated.connect(self.save_current_line)
+
+    def save_current_line(self):
+        print('Save')
+        if self.current_textline.textline != self.label_text.text():
+            self.current_textline.textline = self.label_text.text()
+            self.current_image_dir.save()
+
 
     def jump_to_line_index(self):
         step = int(self.current_line_index.text())
@@ -242,7 +298,7 @@ class App(QMainWindow):
         acc_index = min(int(self.current_acc_index_label.text()), len(self.account) - 1)
         line_index = min(int(self.current_line_index.text()), len(self.account[acc_index]) - 1)
         self.acc_file_index = acc_index
-        self.current_account_file = self.account[self.acc_file_index]
+        self.current_image_dir = self.account[self.acc_file_index]
         self.set_step(line_index)
 
     def eventFilter(self, source, event):
@@ -254,22 +310,12 @@ class App(QMainWindow):
         return super().eventFilter(source, event)
 
     def next_image(self):
-        self.save()
         self.rotate_degree = 0
         self.set_step(self.current_index + 1)
 
     def prev_image(self):
-        self.save()
         self.rotate_degree = 0
         self.set_step(self.current_index - 1)
-
-    def on_correct_button_clicked(self):
-        self.current_account_file.check_flags[self.current_index] = 1
-        self.next_image()
-
-    def on_incorrect_button_clicked(self):
-        self.current_account_file.check_flags[self.current_index] = 0
-        self.next_image()
 
     def on_clockwise_button_clicked(self):
         if self.pillow_image.width * self.pillow_image.height == 0:
@@ -278,12 +324,12 @@ class App(QMainWindow):
 
     def rotate_image(self, angle):
         self.rotate_degree += angle
-        image, _, _ = self.current_account_file[self.current_index]
+        _, image = self.current_image_dir[self.current_index]
         image = image.rotate(self.rotate_degree, expand=True)
         self.loadImage(image)
 
     def is_able_to_next(self, step):
-        if step >= len(self.current_account_file):
+        if step >= len(self.current_image_dir):
             if self.acc_file_index == len(self.account) - 1:
                 return False
         return True
@@ -295,23 +341,24 @@ class App(QMainWindow):
         return True
 
     def set_step(self, step):
-        if step >= len(self.current_account_file):
+        if step >= len(self.current_image_dir):
             if self.acc_file_index == len(self.account) - 1:
                 return
             else:    
                 self.acc_file_index += 1
-            self.current_account_file = self.account[self.acc_file_index]
+            self.current_image_dir = self.account[self.acc_file_index]
             step = 0
         elif step < 0:
             if self.acc_file_index == 0:
                 return
             else:    
                 self.acc_file_index -= 1
-            self.current_account_file = self.account[self.acc_file_index]
-            step = len(self.current_account_file) - 1
+            self.current_image_dir = self.account[self.acc_file_index]
+            step = len(self.current_image_dir) - 1
 
         self.current_index = step
-        image, pred, label = self.current_account_file[step]
+        textline, image = self.current_image_dir[step]
+
         if image.size[0] * image.size[1] == 0:
             print(f'Width or height is 0. WxH = {image.size[0]}x{image.size[1]}')
             if self.is_able_to_next(step):
@@ -324,11 +371,12 @@ class App(QMainWindow):
                 print('Done!')
                 exit(0)
 
+        self.current_textline = textline
         self.current_line_index.setText(f'{self.current_index:05d}')
         self.current_acc_index_label.setText(f'{self.acc_file_index:05d}')
-        self.total_line_label.setText(f'{len(self.current_account_file) - 1:05d}')
-        self.pred_text.setText(pred)
-        self.label_text.setText(label)
+        self.total_line_label.setText(f'{len(self.current_image_dir) - 1:05d}')
+        self.pred_text.setText(textline.predict)
+        self.label_text.setText(textline.textline)
         self.loadImage(image)
 
         #### Resize font
@@ -344,7 +392,7 @@ class App(QMainWindow):
             target_rect = self.label_text.contentsRect()
 
             # Be careful which overload of boundingRect() you call.
-            rect = metrics.boundingRect(target_rect, Qt.AlignLeft, label)
+            rect = metrics.boundingRect(target_rect, Qt.AlignLeft, textline.textline)
             if (rect.width() > target_rect.width() or
                     rect.height() > target_rect.height()):
                 max_size = new_size
@@ -375,24 +423,14 @@ class App(QMainWindow):
         self.adjustScrollBar(self.scrollArea.horizontalScrollBar(), 0)
         self.adjustScrollBar(self.scrollArea.verticalScrollBar(), 1.0)
 
-        self.current_path_label.setText(str(self.current_account_file.image_path))
-        message = "{}, {}x{}, Depth: {}".format(self.current_account_file.image_path, self.image.width(), self.image.height(), self.image.depth())
+        self.current_path_label.setText(str(self.current_image_dir.image_path))
+        message = "{}, {}x{}, Depth: {}".format(self.current_image_dir.image_path, self.image.width(), self.image.height(), self.image.depth())
         self.statusBar().showMessage(message)
         return True
 
     def adjustScrollBar(self, scrollBar: QScrollBar, factor: float):
         scrollBar.setValue(int(factor * scrollBar.value()
                                 + ((factor - 1) * scrollBar.pageStep()/2)))
-
-    def save(self):
-        acc_file: AccountFile
-        for acc_file in self.account:
-            textline_incorrect = []
-            for i, (flag, line) in enumerate(zip(acc_file.check_flags, acc_file.textlines)):
-                if flag == 0:
-                    textline_incorrect.append(line)
-            save_path = acc_file.json_path
-            json.dump(textline_incorrect, open(save_path, 'wt', encoding='utf8'))
 
 
 if __name__ == "__main__":
