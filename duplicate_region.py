8b67b92adc38b14272c03d49f68020883d9273e0
Loi Ly <vinhloiit1327@gmail.com>
2021-01-14 13:53:01 +0700

    add depending labels

--- duplicate_region.py
+++ duplicate_region.py
@@ -38,6 +38,91 @@ def find_shape(label_name: str, shapes: List[Dict]) -> Union[Dict, List[Dict]]:
     return results
 
 
+def to_polygon(shape):
+    if len(shape['points']) == 2 and shape['shape_type'] == 'rectangle':
+        p1, p2 = shape['points']
+        x1 = min(p1[0], p2[0])
+        y1 = min(p1[1], p2[1])
+        x2 = max(p1[0], p2[0])
+        y2 = max(p1[1], p2[1])
+        shape['points'] = [
+            [x1, y1],
+            [x2, y1],
+            [x2, y2],
+            [x1, y2],
+        ]
+        shape['shape_type'] = 'polygon'
+
+
+def post_process_shape(shape):
+    if len(shape['points']) == 4 and shape['shape_type'] == 'rectangle':
+        shape['shape_type'] = 'polygon'
+
+
+def map_content(region_ref, region_new, json_ref):
+    region_src = order_points(region_ref['points'])
+    region_dst = order_points(region_new['points'])
+
+    src_array = np.array(region_src, np.float32).reshape(-1, 2)
+    dst_array = np.array(region_dst, np.float32).reshape(-1, 2)
+    M: np.ndarray = cv2.getPerspectiveTransform(src_array, dst_array)
+
+    new_shapes = []
+
+    for shape in json_ref['shapes']:
+        if shape['label'] == region_new['label']:
+            continue
+
+        to_polygon(shape)
+        shape_points = order_points(shape['points'])
+        ratio = compute_intersection_ratio(region_src, shape_points)
+        if ratio >= 0.6:
+            src_array = np.array(shape_points, dtype=np.float32).reshape(-1, 2)
+            dst_array = cv2.perspectiveTransform(np.array([src_array]), M).squeeze(0)
+            dst = dst_array.tolist()
+
+            dst_shape = copy.deepcopy(shape)
+            dst_shape['points'] = dst
+            post_process_shape(dst_shape)
+            new_shapes.append(dst_shape)
+
+    return new_shapes
+
+
+def map_based(region_ref, region_new, json_ref, depending_items: List[str]):
+
+    if len(depending_items) == 0:
+        return []
+
+    region_src = order_points(region_ref['points'])
+    region_dst = order_points(region_new['points'])
+
+    src_array = np.array(region_src, np.float32).reshape(-1, 2)
+    dst_array = np.array(region_dst, np.float32).reshape(-1, 2)
+    M: np.ndarray = cv2.getPerspectiveTransform(src_array, dst_array)
+
+    new_shapes = []
+
+    for shape in json_ref['shapes']:
+        if shape['label'] == region_new['label']:
+            continue
+
+        if shape['label'] in depending_items:
+            to_polygon(shape)
+            shape_points = order_points(shape['points'])
+
+            src_array = np.array(shape_points, dtype=np.float32).reshape(-1, 2)
+            dst_array = cv2.perspectiveTransform(np.array([src_array]), M).squeeze(0)
+            dst = dst_array.tolist()
+
+            dst_shape = copy.deepcopy(shape)
+            dst_shape['points'] = dst
+            post_process_shape(dst_shape)
+            new_shapes.append(dst_shape)
+
+    return new_shapes
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('ref_json', type=str, help='Reference json file which will be duplicated for each image')
@@ -76,33 +161,21 @@ if __name__ == "__main__":
         region_news = [shape for shape in json_new['shapes'] if shape['label'] in region_config['names']]
         json_new['shapes'] = copy.deepcopy(region_news)
         for region_new in region_news:
-            found = False
             for region_ref in json_ref['shapes']:
                 if region_ref['label'] != region_new['label']:
                     continue
-                found = True
-                region_src = order_points(region_ref['points'])
-                region_dst = order_points(region_new['points'])
+                new_shapes = map_content(region_ref, region_new, json_ref)
+                json_new['shapes'].extend(new_shapes)
 
-                src_array = np.array(region_src, np.float32).reshape(-1, 2)
-                dst_array = np.array(region_dst, np.float32).reshape(-1, 2)
-                M: np.ndarray = cv2.getPerspectiveTransform(src_array, dst_array)
+                if 'depend' in region_config.keys():
+                    dependence = region_config['depend']
+                    for depend_region_name, depend_labels in dependence.items():
+                        if depend_region_name != region_ref['label']:
+                            continue
+                        new_shapes = map_based(region_ref, region_new, json_ref, depend_labels)
+                        json_new['shapes'].extend(new_shapes)
 
-                for shape in json_ref['shapes']:
-                    if shape['label'] == region_new['label']:
-                        continue
 
-                    shape_points = order_points(shape['points'])
-                    ratio = compute_intersection_ratio(region_src, shape_points)
-                    if ratio >= 0.6:
-                        src_array = np.array(shape_points, dtype=np.float32).reshape(-1, 2)
-                        dst_array = cv2.perspectiveTransform(np.array([src_array]), M).squeeze(0)
-                        dst = dst_array.tolist()
-
-                        dst_shape = copy.deepcopy(shape)
-                        dst_shape['points'] = dst
-
-                        json_new['shapes'].append(dst_shape)
         image_path = json_path.with_suffix(f'.{args.ext}')
         json_new['imagePath'] = image_path.name
         json_new['imageWidth'], json_new['imageHeight'] = Image.open(image_path).size
