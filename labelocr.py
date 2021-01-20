e10b8484b517808ecf1a017fdceb15c5840032d4
Loi Ly <vinhloiit1327@gmail.com>
2021-01-20 17:52:44 +0700

    refactor layout

--- labelocr.py
+++ labelocr.py
@@ -1,6 +1,6 @@
 from typing import Dict
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QAction, QHBoxLayout, QSizePolicy, QToolBar, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QAction, QSizePolicy, QToolBar, QWidget, QGridLayout
 from data import Dataset
 from widgets.shape_editor import ShapeEditor
 from widgets.annotation_viewer import AnnotationViewer
@@ -12,52 +12,30 @@ class LabelOCR(QWidget):
     def __init__(self, dataset: Dataset, config: Dict):
         super().__init__()
         self.dataset = dataset
-        left_layout = QVBoxLayout()
 
         index_selector = IndexWidget(self.dataset)
         index_selector.on_change.connect(self.dataset.itemAt)
         self.dataset.indexChanged.connect(index_selector.updateIndex)
-        left_layout.addWidget(index_selector)
 
         annotationViewer = AnnotationViewer(config['labels'])
         annotationViewer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        left_layout.addWidget(annotationViewer)
 
         annotationViewer.setAnnotation(self.dataset[0][0], self.dataset[0][1])
-
-        right_layout = QVBoxLayout()
 
         shapeEditor = ShapeEditor()
         shapeEditor.setMinimumWidth(600)
         shapeEditor.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        right_layout.addWidget(shapeEditor)
         annotationViewer.shape.connect(shapeEditor.setShape)
 
-        # self.dataset.item.connect(index_selector.update_card_index)
         self.dataset.item.connect(annotationViewer.setAnnotation)
 
-        layout = QHBoxLayout(self)
-
-        left_widget = QWidget()
-        left_widget.setLayout(left_layout)
-        left_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        layout.addWidget(left_widget)
-
-        right_widget = QWidget()
-        right_widget.setLayout(right_layout)
-        # right_widget.setFixedWidth(800)
-        right_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        layout.addWidget(right_widget)
-        self.adjustSize()
-        # self.showMaximized()
-
-        # # set the first line of the first card
         self.dataset.itemAt(0)
 
         ##################
         # Init Menubar
         ##################
-        self.toolbar = QToolBar('Toolbar', self)
+        toolbar = QToolBar('Toolbar')
+        toolbar.setOrientation(Qt.Vertical)
         # rotateAction = QAction('&Rotate', self)
         # rotateAction.setShortcut('Ctrl+R')
         # rotateAction.triggered.connect(annotationViewer.imageViewer.rotateImage)
@@ -84,4 +62,10 @@ class LabelOCR(QWidget):
         prevShapeAction.setShortcut(Qt.Key_Up)
         prevShapeAction.triggered.connect(annotationViewer.prev)
 
-        self.toolbar.addActions([nextImageAction, prevImageAction, nextShapeAction, prevShapeAction])
+        toolbar.addActions([nextImageAction, prevImageAction, nextShapeAction, prevShapeAction])
+
+        layout = QGridLayout(self)
+        layout.addWidget(toolbar, 0, 0, 2, 1)
+        layout.addWidget(index_selector, 0, 1, 1, 2)
+        layout.addWidget(annotationViewer, 1, 1, 1, 1)
+        layout.addWidget(shapeEditor, 1, 2, 1, 1)
